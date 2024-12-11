2ec4251b59e4003dcca2e7a0e8b84d60054f2bd7
MBotond21 <m.botond07@gmail.com>
2024-12-11 17:17:43 +0100

    final api service

--- app/src/main/java/com/example/bevasarlas/RetrofitApiService.java
+++ app/src/main/java/com/example/bevasarlas/RetrofitApiService.java
@@ -3,11 +3,22 @@ package com.example.bevasarlas;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface RetrofitApiService {
 
     @GET("lFdUM1/data")
     Call<List<Product>> getProducts();
+
+    @POST("acttFJ/data")
+    Call<Product> createProduct(@Body Product product);
+
+    @DELETE("acttFJ/data/{id}")
+    Call<Void> deleteProduct(@Path("id") int id);
 
 }
